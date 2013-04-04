c73b3c85c8f6a742c4076a0ea7cbaade7e5b7af4
Knut <knut@kokonut.no>
2013-04-04 17:34:39 +0200

    Minor text changes.

--- filmbyen/index.php
+++ filmbyen/index.php
@@ -14,7 +14,7 @@
         </div>
         <h1>Filmbyen</h1>
         <p class="intro">Et 8 år langt hobbyprosjekt. Jeg sto for design, frontend og mye av innholdet forøvrig. I bunnen lå et spesialtilpasset CMS - bygd i tett samarbeid med <i>Joacim Lunde</i>. Sidene ble lukket i slutten av 2012.</p>
-        <p>Filmbyen ble startet i 2004. På den tiden fantes det ikke en fullverdig norsk filmside som ga filminteresserte en arena å diskutere og lese om film på nett. Målet med nettsiden var helt siden lansering å tette dette hullet.</p>
+        <p>Filmbyen ble startet i 2004. På den tiden fantes det ikke en fullverdig norsk filmside som ga filminteresserte en arena å både diskutere og lese om film på nett. Målet med nettsiden var helt siden lansering å tette dette hullet.</p>
         <p>På sitt største hadde Filmbyen over 2 000 unike besøkende daglig. Vi anmeldte rundt 30 kino og DVD-filmer i måneden - hvorav noen ble printet i Byavisa. Vi hadde en frivillig redaksjon på nærmere 20 stykk. Vi gjorde intervjuer med cast og crew fra flere norske storproduksjoner - blant annet <i>Max Manus</i> og <i>Fritt Vilt</i>. I tillegg hadde vi en jevn strøm av egenproduserte artikler og nyhetssaker.</p>
         <p>Siden lanseringen av Filmbyen i 2004 gjennomgikk sidene en rekke ansiktsløftninger. Nedenfor finner du en kronologisk utlisting av de forskjellige designene.<br/><br/></p>
         <div class="gallery">
